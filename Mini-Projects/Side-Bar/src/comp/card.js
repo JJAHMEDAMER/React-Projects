e02a65c821a4379160f8ebc8b5fbb58a3b754fc6
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-11 17:09:09 +0200

    card template

--- Mini-Projects/Side-Bar/src/comp/card.js
+++ Mini-Projects/Side-Bar/src/comp/card.js
@@ -1,8 +1,22 @@
 import React from "react";
 
-const Card = () => (
-  <div>
-    <h1>Card Hello World</h1>
+//assets
+import right from "../assets/right.png";
+
+const Card = (props) => (
+  <div className="card">
+    <h1 className="card--title">{props.title}</h1>
+    <p className="card--company">{props.company}</p>
+    <p className="card--date">{props.dates}</p>
+
+    {props.duties.map((item) => {
+      return (
+        <div className="card--duties">
+          <img src={right} className="card--duties--img" alt="chevron right" />
+          <p className="card--duties--text">{item}</p>
+        </div>
+      );
+    })}
   </div>
 );
 

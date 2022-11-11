83116045082dcddceebb786077190e8c60edf122
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-11 18:40:36 +0200

    navbar

--- Mini-Projects/Side-Bar/src/App.js
+++ Mini-Projects/Side-Bar/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 // Comp
 import Card from "./comp/card";
+import NavBar from "./comp/NavBar";
 
 // Fetch Data
 const url = "https://course-api.com/react-tabs-project";
@@ -19,11 +20,21 @@ function App() {
   }, []);
 
   const cardList = data.map((item) => <Card {...item} />);
+
+  function navbarFunc() {
+    alert("worklink");
+  }
+  const navBarList = data.map((item) => (
+    <NavBar comp={item["company"]} onClickNavbar={navbarFunc} />
+  ));
+
   return (
     <div className="app">
       <h1 className="app--title">Experience</h1>
-      <div className="app--card">
-        {cardList[0]}
+      <div className="app--content">
+        <div className="app--navbar">{navBarList}</div>
+        <div className="app--divider"></div>
+        <div className="app--card">{cardList[0]}</div>
       </div>
     </div>
   );
---
+++ Mini-Projects/Side-Bar/src/comp/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavBar = ({ comp, onClickNavbar }) => {
+  return (
+    <button className="navbar" onClick={onClickNavbar}>
+      <p className="navbar--text">{comp}</p>
+      <div className="navbar--underline"></div>
+    </button>
+  );
+};
+
+export default NavBar;

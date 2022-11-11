3188a7f1944acc8b809e54f6d6f8337f5fae0ca1
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-11 16:51:09 +0200

    fetch data

--- Mini-Projects/Side-Bar/src/App.js
+++ Mini-Projects/Side-Bar/src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // Comp
-import Card from "./comp/card"
+import Card from "./comp/card";
+
+// Fetch Data
+const url = "https://course-api.com/react-tabs-project";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  async function APIRequest() {
+    const response = await fetch(url);
+    setData(await response.json());
+  }
+
+  console.log(data);
+
+  useEffect(() => {
+    APIRequest();
+  }, []);
+
+  console.log(data);
+
+  const cardList = data.map((item) => <Card {...item} />);
+
   return (
     <div>
       <h1>App Hello World</h1>
-      <Card/>
+      {cardList}
     </div>
   );
 }

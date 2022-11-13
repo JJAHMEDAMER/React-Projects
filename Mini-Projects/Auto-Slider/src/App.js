ce5af6e6b07a1500a74f93d9a644276b649f14ab
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-13 19:41:33 +0200

    button functionality

--- Mini-Projects/Auto-Slider/src/App.js
+++ Mini-Projects/Auto-Slider/src/App.js
@@ -7,16 +7,46 @@ import data from "./data";
 import Card from "./comp/card";
 
 // image
-import next from "./assets/next.png";
+import nextChevron from "./assets/next.png";
 
 function App() {
+  const [index, setIndex] = React.useState(0);
+
+  function next() {
+    if (index >= data.length - 1) {
+      setIndex(0);
+    } else {
+      let newIndex = index + 1;
+      setIndex(newIndex);
+    }
+  }
+
+  function back() {
+    if (index <= 0) {
+      setIndex(data.length - 1);
+    } else {
+      let newIndex = index - 1;
+      setIndex(newIndex);
+    }
+  }
+
   return (
     <div className="app">
-      <img className="app--back" src={next} alt="backward" />
+      <img
+        className="app--back"
+        src={nextChevron}
+        alt="backward"
+        onClick={back}
+      />
       <div>
-        <Card {...data[0]} />
+        <Card {...data[index]} />
       </div>
-      <img className="app--next" src={next} alt="forward" />
+      <img
+        className="app--next"
+        src={nextChevron}
+        alt="forward"
+        onClick={next}
+      />
     </div>
   );
 }

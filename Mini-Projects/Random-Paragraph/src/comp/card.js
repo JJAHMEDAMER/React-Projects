ad63f66151fc52a22f4680de01633e0796ebfd4f
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-14 15:52:40 +0200

    Card control row

--- Mini-Projects/Random-Paragraph/src/comp/card.js
+++ Mini-Projects/Random-Paragraph/src/comp/card.js
@@ -2,6 +2,13 @@ import React from "react";
 
 const Card = () => (
   <div className="card">
+    <div className="card--control">
+      <div className="card--control">
+        <p className="card--control--title">Paragraph: </p>
+        <input className="card--control--input" placeholder="1"></input>
+      </div>
+      <button className="card--control--button">Generate</button>
+    </div>
     <h1>Card Hello World</h1>
   </div>
 );

992e7f49f36551336a54edb1b529f7c4ab46ca39
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-15 13:14:44 +0200

    Color Generator Start

---
+++ Mini-Projects/Color-Generator/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//Style
+import "./App.css"
+
+//comp
+import App from "./App"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById("root")
+);

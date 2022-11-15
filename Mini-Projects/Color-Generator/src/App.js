65f4c338a95b85577fbaedd5c276b06093a7f2ef
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-15 14:07:05 +0200

    hex to rgb

--- Mini-Projects/Color-Generator/src/App.js
+++ Mini-Projects/Color-Generator/src/App.js
@@ -6,16 +6,31 @@ import ControllerBar from "./comp/controller";
 
 function App() {
   let color;
+  const [colorList, setColorList] = React.useState([]);
   function getInput(changedValue) {
     color = changedValue;
   }
 
+  function generate() {
+    let rgb = hexToRgb(color);
+    console.log(rgb);
+  }
 
+  function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result
+      ? [
+          parseInt(result[1], 16),
+          parseInt(result[2], 16),
+          parseInt(result[3], 16),
+        ]
+      : null;
+  }
 
   return (
     <div>
       <h1>App Hello World</h1>
-      <ControllerBar onChangeHandler={getInput} />
+      <ControllerBar onClickHandler={generate} onChangeHandler={getInput} />
       <Card />
     </div>
   );

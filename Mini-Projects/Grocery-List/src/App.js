dda092de72dc2f7a377794b235e8a4bdfbad0b0c
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-16 18:32:56 +0200

    controller

--- Mini-Projects/Grocery-List/src/App.js
+++ Mini-Projects/Grocery-List/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 // Comp
-import Card from "./comp/card"
+import Card from "./comp/card";
+import ControllerBar from "./comp/controller";
 
 function App() {
+  function add() {}
+
+  let inputValue;
+  function getInput(changedValue) {
+    inputValue = changedValue;
+  }
   return (
     <div>
       <h1>App Hello World</h1>
-      <Card/>
+      <Card />
+      <ControllerBar onClickHandler={add} onChangeHandler={getInput} />
     </div>
   );
 }

1a2ad4d4497625e64a5e574c3ef97bfa131ce9a9
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-16 18:08:19 +0200

    React 18

--- Mini-Projects/Grocery-List/src/index.js
+++ Mini-Projects/Grocery-List/src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 //Style
-import "./App.css"
+import "./App.css";
 
 //comp
-import App from "./App"
+import App from "./App";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <App />
+  </React.StrictMode>
 );

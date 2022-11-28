6e2425602de8c063f2e70cf27c2d8c8d0c220644
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-28 21:23:45 +0200

    import React router

--- Full-Projects/Chat-App/src/index.js
+++ Full-Projects/Chat-App/src/index.js
@@ -1,11 +1,20 @@
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from 'components';
+import "./index.css";
+
+// React
+import React from "react";
+import ReactDOM from "react-dom";
+
+// React Router
+import { BrowserRouter as Router } from "react-router-dom";
+
+// Components
+import { App } from "components";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );

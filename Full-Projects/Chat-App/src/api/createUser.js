21e3363226046b861a83933d0d7c667613f7aa6a
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-28 23:47:52 +0200

    vercel setup and create user

---
+++ Full-Projects/Chat-App/src/api/createUser.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const createUser = async (req, res) => {
+  const { userId, userName } = req.body;
+
+  axios
+    .post(
+      "https://api.chatengine.io/projects/people/",
+      {
+        username: userName,
+        secret: userId,
+      },
+      { headers: { "private-Key": process.env.chat_engine_private_key } }
+    )
+    .then((apiRes) => {
+      res.json({
+        body: apiRes.body,
+        error: null,
+      });
+    })
+    .catch(() => {
+      res.json({
+        body: null,
+        error: "Big Error",
+      });
+    });
+};
+
+export default createUser;

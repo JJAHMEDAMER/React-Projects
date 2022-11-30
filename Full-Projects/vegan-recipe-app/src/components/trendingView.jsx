f8c69e30fb24eab57891157ad13371ffa96521f5
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-30 04:53:21 +0200

    fetch data from api and set state

--- Full-Projects/vegan-recipe-app/src/components/trendingView.jsx
+++ Full-Projects/vegan-recipe-app/src/components/trendingView.jsx
@@ -1,5 +1,29 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+// Comp
+import { Card } from "./card"
+
 export const TrendingView = () => {
+
+    const [trendingApiRes, setTrendingApiRes] = useState([])
+
+    async function getTrendingRes() {
+        const URL = `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=9`
+        const apiResString = await fetch(URL);
+        const apiResJson = await apiResString.json()
+        console.log(apiResJson)
+        setTrendingApiRes(apiResJson.recipes)
+    }
+
+    // useEffect(() => {
+    //     getTrendingRes();
+    // },[])
+
     return <div>
-        <h1>Trending</h1>
+        <h1 onClick={getTrendingRes}>Trending</h1>
+        <div>
+            {trendingApiRes.map((item) => (<Card {...item} key={item.id} />))}
+        </div>
     </div>
 }

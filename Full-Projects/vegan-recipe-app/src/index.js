220742a04a3f83fdf040a110dc2ed6b240ff9e1e
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-01 01:51:24 +0200

    install react router and use browser Router

--- Full-Projects/vegan-recipe-app/src/index.js
+++ Full-Projects/vegan-recipe-app/src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDom from "react-dom/client"; // React 18
 
-// Style 
+import { BrowserRouter } from "react-router-dom";
+
+// Style
 import "./index.css";
 
 // App
 import { App } from "./App";
 
 const root = document.getElementById("root");
-const container = ReactDom.createRoot(root); 
+const container = ReactDom.createRoot(root);
 
 container.render(
   <React.StrictMode>
-    <App/>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );

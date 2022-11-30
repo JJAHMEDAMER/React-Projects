2835a9227e01dc364208c69b528f2782b83624a5
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-30 21:40:37 +0200

    fake data for development to not use up api quota

--- Full-Projects/vegan-recipe-app/src/components/trendingView.jsx
+++ Full-Projects/vegan-recipe-app/src/components/trendingView.jsx
@@ -4,21 +4,25 @@ import { useState } from "react"
 // Comp
 import { Card } from "./card"
 
+// FakeData
+import { fakeData } from "../fakeData"
+
 export const TrendingView = () => {
 
     const [trendingApiRes, setTrendingApiRes] = useState([])
 
     async function getTrendingRes() {
-        const URL = `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=9`
+        const URL = `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=1`
         const apiResString = await fetch(URL);
         const apiResJson = await apiResString.json()
         console.log(apiResJson)
         setTrendingApiRes(apiResJson.recipes)
     }
 
-    // useEffect(() => {
-    //     getTrendingRes();
-    // },[])
+    useEffect(() => {
+        // getTrendingRes();
+        setTrendingApiRes(fakeData)
+    }, [])
 
     return <div>
         <h1 onClick={getTrendingRes}>Trending</h1>
---
+++ Full-Projects/vegan-recipe-app/src/fakeData.js
@@ -0,0 +1,376 @@
+export const fakeData = [
+    {
+        "vegetarian": false,
+        "vegan": false,
+        "glutenFree": true,
+        "dairyFree": false,
+        "veryHealthy": false,
+        "cheap": false,
+        "veryPopular": false,
+        "sustainable": false,
+        "lowFodmap": false,
+        "weightWatcherSmartPoints": 2,
+        "gaps": "no",
+        "preparationMinutes": -1,
+        "cookingMinutes": -1,
+        "aggregateLikes": 97,
+        "healthScore": 2,
+        "creditsText": "Foodista.com – The Cooking Encyclopedia Everyone Can Edit",
+        "license": "CC BY 3.0",
+        "sourceName": "Foodista",
+        "pricePerServing": 105.29,
+        "extendedIngredients": [
+            {
+                "id": 11143,
+                "aisle": "Produce",
+                "image": "celery.jpg",
+                "consistency": "SOLID",
+                "name": "celery",
+                "nameClean": "celery",
+                "original": "1/2 stick celery, finely chopped",
+                "originalName": "celery, finely chopped",
+                "amount": 0.5,
+                "unit": "stick",
+                "meta": [
+                    "finely chopped"
+                ],
+                "measures": {
+                    "us": {
+                        "amount": 0.5,
+                        "unitShort": "stick",
+                        "unitLong": "sticks"
+                    },
+                    "metric": {
+                        "amount": 0.5,
+                        "unitShort": "stick",
+                        "unitLong": "sticks"
+                    }
+                }
+            },
+            {
+                "id": 1002046,
+                "aisle": "Condiments",
+                "image": "dijon-mustard.jpg",
+                "consistency": "LIQUID",
+                "name": "dijon mustard",
+                "nameClean": "creole mustard",
+                "original": "1 tablespoon Dijon mustard",
+                "originalName": "Dijon mustard",
+                "amount": 1,
+                "unit": "tablespoon",
+                "meta": [],
+                "measures": {
+                    "us": {
+                        "amount": 1,
+                        "unitShort": "Tbsp",
+                        "unitLong": "Tbsp"
+                    },
+                    "metric": {
+                        "amount": 1,
+                        "unitShort": "Tbsp",
+                        "unitLong": "Tbsp"
+                    }
+                }
+            },
+            {
+                "id": 1123,
+                "aisle": "Milk, Eggs, Other Dairy",
+                "image": "egg.png",
+                "consistency": "SOLID",
+                "name": "eggs",
+                "nameClean": "egg",
+                "original": "6 hard boiled eggs, peeled, halved, and yolks removed",
+                "originalName": "hard boiled eggs, peeled, halved, and yolks removed",
+                "amount": 6,
+                "unit": "",
+                "meta": [
+                    "hard",
+                    "boiled",
+                    "peeled",
+                    "halved"
+                ],
+                "measures": {
+                    "us": {
+                        "amount": 6,
+                        "unitShort": "",
+                        "unitLong": ""
+                    },
+                    "metric": {
+                        "amount": 6,
+                        "unitShort": "",
+                        "unitLong": ""
+                    }
+                }
+            },
+            {
+                "id": 11156,
+                "aisle": "Produce",
+                "image": "fresh-chives.jpg",
+                "consistency": "SOLID",
+                "name": "fresh chives",
+                "nameClean": "chives",
+                "original": "3 tablespoons chopped fresh chives",
+                "originalName": "chopped fresh chives",
+                "amount": 3,
+                "unit": "tablespoons",
+                "meta": [
+                    "fresh",
+                    "chopped"
+                ],
+                "measures": {
+                    "us": {
+                        "amount": 3,
+                        "unitShort": "Tbsps",
+                        "unitLong": "Tbsps"
+                    },
+                    "metric": {
+                        "amount": 3,
+                        "unitShort": "Tbsps",
+                        "unitLong": "Tbsps"
+                    }
+                }
+            },
+            {
+                "id": 9152,
+                "aisle": "Produce",
+                "image": "lemon-juice.jpg",
+                "consistency": "LIQUID",
+                "name": "lemon juice",
+                "nameClean": "lemon juice",
+                "original": "1 tablespoon fresh lemon juice",
+                "originalName": "fresh lemon juice",
+                "amount": 1,
+                "unit": "tablespoon",
+                "meta": [
+                    "fresh"
+                ],
+                "measures": {
+                    "us": {
+                        "amount": 1,
+                        "unitShort": "Tbsp",
+                        "unitLong": "Tbsp"
+                    },
+                    "metric": {
+                        "amount": 1,
+                        "unitShort": "Tbsp",
+                        "unitLong": "Tbsp"
+                    }
+                }
+            },
+            {
+                "id": 10115136,
+                "aisle": "Seafood",
+                "image": "lump-crabmeat.png",
+                "consistency": "SOLID",
+                "name": "lump crabmeat",
+                "nameClean": "lump crabmeat",
+                "original": "4 ounces lump crabmeat",
+                "originalName": "lump crabmeat",
+                "amount": 4,
+                "unit": "ounces",
+                "meta": [],
+                "measures": {
+                    "us": {
+                        "amount": 4,
+                        "unitShort": "oz",
+                        "unitLong": "ounces"
+                    },
+                    "metric": {
+                        "amount": 113.398,
+                        "unitShort": "g",
+                        "unitLong": "grams"
+                    }
+                }
+            },
+            {
+                "id": 4025,
+                "aisle": "Condiments",
+                "image": "mayonnaise.png",
+                "consistency": "LIQUID",
+                "name": "mayonnaise",
+                "nameClean": "mayonnaise",
+                "original": "1 tablespoon mayonnaise",
+                "originalName": "mayonnaise",
+                "amount": 1,
+                "unit": "tablespoon",
+                "meta": [],
+                "measures": {
+                    "us": {
+                        "amount": 1,
+                        "unitShort": "Tbsp",
+                        "unitLong": "Tbsp"
+                    },
+                    "metric": {
+                        "amount": 1,
+                        "unitShort": "Tbsp",
+                        "unitLong": "Tbsp"
+                    }
+                }
+            },
+            {
+                "id": 1102047,
+                "aisle": "Spices and Seasonings",
+                "image": "salt-and-pepper.jpg",
+                "consistency": "SOLID",
+                "name": "salt and pepper",
+                "nameClean": "salt and pepper",
+                "original": "Salt and freshly ground black pepper",
+                "originalName": "Salt and freshly ground black pepper",
+                "amount": 6,
+                "unit": "servings",
+                "meta": [
+                    "black",
+                    "freshly ground"
+                ],
+                "measures": {
+                    "us": {
+                        "amount": 6,
+                        "unitShort": "servings",
+                        "unitLong": "servings"
+                    },
+                    "metric": {
+                        "amount": 6,
+                        "unitShort": "servings",
+                        "unitLong": "servings"
+                    }
+                }
+            },
+            {
+                "id": 1056,
+                "aisle": "Milk, Eggs, Other Dairy",
+                "image": "sour-cream.jpg",
+                "consistency": "SOLID",
+                "name": "sour cream",
+                "nameClean": "sour cream",
+                "original": "2-3 tablespoons sour cream",
+                "originalName": "sour cream",
+                "amount": 2,
+                "unit": "tablespoons",
+                "meta": [
+                    "sour"
+                ],
+                "measures": {
+                    "us": {
+                        "amount": 2,
+                        "unitShort": "Tbsps",
+                        "unitLong": "Tbsps"
+                    },
+                    "metric": {
+                        "amount": 2,
+                        "unitShort": "Tbsps",
+                        "unitLong": "Tbsps"
+                    }
+                }
+            }
+        ],
+        "id": 641461,
+        "title": "Deviled Eggs With Crab",
+        "readyInMinutes": 45,
+        "servings": 6,
+        "sourceUrl": "http://www.foodista.com/recipe/SG5QX2ZK/deviled-eggs-with-crab",
+        "image": "https://spoonacular.com/recipeImages/641461-556x370.jpg",
+        "imageType": "jpg",
+        "summary": "Deviled Eggs With Crab might be just the <b>American</b> recipe you are searching for. This recipe makes 6 servings with <b>106 calories</b>, <b>9g of protein</b>, and <b>7g of fat</b> each. For <b>$1.05 per serving</b>, this recipe <b>covers 8%</b> of your daily requirements of vitamins and minerals. It works well as a budget friendly hor d'oeuvre. If you have celery, chives, eggs, and a few other ingredients on hand, you can make it. A few people made this recipe, and 96 would say it hit the spot. It is a good option if you're following a <b>gluten free and pescatarian</b> diet. From preparation to the plate, this recipe takes around <b>45 minutes</b>. All things considered, we decided this recipe <b>deserves a spoonacular score of 40%</b>. This score is not so great. Try <a href=\"https://spoonacular.com/recipes/crab-deviled-eggs-395496\">Crab Deviled Eggs</a>, <a href=\"https://spoonacular.com/recipes/deviled-eggs-with-crab-310862\">Deviled Eggs with Crab</a>, and <a href=\"https://spoonacular.com/recipes/crab-deviled-eggs-476709\">Crab Deviled Eggs</a> for similar recipes.",
+        "cuisines": [
+            "American"
+        ],
+        "dishTypes": [
+            "side dish"
+        ],
+        "diets": [
+            "gluten free",
+            "pescatarian"
+        ],
+        "occasions": [],
+        "instructions": "<ol><li>In a medium-sized mixing bowl combine the crabmeat, celery, sour cream, mayonnaise, Dijon mustard, lemon juice, and chives. Stir until well combined.</li><li>Season, to taste, with salt and pepper.</li><li>Spoon the crab mixture into the egg halves.</li><li>Serve immediately or chilled until ready.</li></ol>",
+        "analyzedInstructions": [
+            {
+                "name": "",
+                "steps": [
+                    {
+                        "number": 1,
+                        "step": "In a medium-sized mixing bowl combine the crabmeat, celery, sour cream, mayonnaise, Dijon mustard, lemon juice, and chives. Stir until well combined.Season, to taste, with salt and pepper.Spoon the crab mixture into the egg halves.",
+                        "ingredients": [
+                            {
+                                "id": 1102047,
+                                "name": "salt and pepper",
+                                "localizedName": "salt and pepper",
+                                "image": "salt-and-pepper.jpg"
+                            },
+                            {
+                                "id": 1032046,
+                                "name": "dijon mustard",
+                                "localizedName": "dijon mustard",
+                                "image": "dijon-mustard.jpg"
+                            },
+                            {
+                                "id": 9152,
+                                "name": "lemon juice",
+                                "localizedName": "lemon juice",
+                                "image": "lemon-juice.jpg"
+                            },
+                            {
+                                "id": 4025,
+                                "name": "mayonnaise",
+                                "localizedName": "mayonnaise",
+                                "image": "mayonnaise.png"
+                            },
+                            {
+                                "id": 1056,
+                                "name": "sour cream",
+                                "localizedName": "sour cream",
+                                "image": "sour-cream.jpg"
+                            },
+                            {
+                                "id": 10015136,
+                                "name": "crabmeat",
+                                "localizedName": "crabmeat",
+                                "image": "crabmeat.jpg"
+                            },
+                            {
+                                "id": 11143,
+                                "name": "celery",
+                                "localizedName": "celery",
+                                "image": "celery.jpg"
+                            },
+                            {
+                                "id": 11156,
+                                "name": "chives",
+                                "localizedName": "chives",
+                                "image": "fresh-chives.jpg"
+                            },
+                            {
+                                "id": 15136,
+                                "name": "crab",
+                                "localizedName": "crab",
+                                "image": "crabmeat.jpg"
+                            },
+                            {
+                                "id": 1123,
+                                "name": "egg",
+                                "localizedName": "egg",
+                                "image": "egg.png"
+                            }
+                        ],
+                        "equipment": [
+                            {
+                                "id": 405907,
+                                "name": "mixing bowl",
+                                "localizedName": "mixing bowl",
+                                "image": "mixing-bowl.jpg"
+                            }
+                        ]
+                    },
+                    {
+                        "number": 2,
+                        "step": "Serve immediately or chilled until ready.",
+                        "ingredients": [],
+                        "equipment": []
+                    }
+                ]
+            }
+        ],
+        "originalId": null,
+        "spoonacularSourceUrl": "https://spoonacular.com/deviled-eggs-with-crab-641461"
+    }
+]

68c5e44cc73c8f3af3ec22f3ed7ae22ee401c440
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-06 10:59:29 +0200

    hero section content && styling

--- Full-Projects/portofolio-01/src/components/hero/hero.jsx
+++ Full-Projects/portofolio-01/src/components/hero/hero.jsx
@@ -1,8 +1,35 @@
+//Dependencies
+import { GrLinkedin } from "react-icons/gr"
+import { FaGithubSquare, FaLinkedin, } from "react-icons/fa"
+
 // Styling
 import "./hero.css"
 
+// Images
+import heroImg from "../../assets/me.png"
+
 export const Hero = () => {
-    return <div className="hero">
-        <h1>Hero</h1>
+    return <div className="section">
+        <div className="hero">
+            <h2>Hello I'm</h2>
+            <h1>Ahmed Amer</h1>
+            <h3>Fullstack Developer</h3>
+            <div className="v-3rem"></div>
+            <div className="hero--btns">
+                <button className="sec-btn">Download CV</button>
+                <button> Let's Talk</button>
+            </div>
+            <div className="v-3rem"></div>
+            <div className="hero--content">
+                <div className="hero--icons">
+                    <FaLinkedin />
+                    <FaGithubSquare />
+                    <div className="line"></div>
+                </div>
+                <img src={heroImg} alt="ahmed-amer-img" className="hero--img" />
+                <p className="hero--sroll--down">SCROLL DOWN</p>
+            </div>
+
+        </div>
     </div>
 }

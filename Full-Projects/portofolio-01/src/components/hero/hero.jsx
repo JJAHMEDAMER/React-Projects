aea8597932c5b134f3081442fbe75329296928aa
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-06 12:24:12 +0200

    organize css for reusable components

--- Full-Projects/portofolio-01/src/components/hero/hero.jsx
+++ Full-Projects/portofolio-01/src/components/hero/hero.jsx
@@ -1,35 +1,32 @@
 //Dependencies
-import { GrLinkedin } from "react-icons/gr"
 import { FaGithubSquare, FaLinkedin, } from "react-icons/fa"
 
 // Styling
 import "./hero.css"
 
-// Images
+// Assets
 import heroImg from "../../assets/me.png"
 
 export const Hero = () => {
     return <div className="section">
-        <div className="hero">
-            <h2>Hello I'm</h2>
-            <h1>Ahmed Amer</h1>
-            <h3>Fullstack Developer</h3>
-            <div className="v-3rem"></div>
-            <div className="hero--btns">
-                <button className="sec-btn">Download CV</button>
-                <button> Let's Talk</button>
-            </div>
-            <div className="v-3rem"></div>
-            <div className="hero--content">
-                <div className="hero--icons">
-                    <FaLinkedin />
-                    <FaGithubSquare />
-                    <div className="line"></div>
-                </div>
-                <img src={heroImg} alt="ahmed-amer-img" className="hero--img" />
-                <p className="hero--sroll--down">SCROLL DOWN</p>
-            </div>
-
+        <h2 className="section--intro">Hello I'm</h2>
+        <h1 className="section--title">Ahmed Amer</h1>
+        <h3 className="section--tagline">Fullstack Developer</h3>
+        <div className="v-3rem"></div>
+        <div className="hero--btns">
+            <button className="sec-btn">Download CV</button>
+            <button> Let's Talk</button>
         </div>
+        <div className="v-3rem"></div>
+        <div className="hero--content">
+            <div className="hero--icons">
+                <FaLinkedin />
+                <FaGithubSquare />
+                <div className="line"></div>
+            </div>
+            <img src={heroImg} alt="ahmed-amer-img" className="hero--img" />
+            <p className="hero--sroll--down">SCROLL DOWN</p>
+        </div>
+
     </div>
 }
